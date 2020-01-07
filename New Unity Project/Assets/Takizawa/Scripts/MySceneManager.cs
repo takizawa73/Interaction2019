c1d4af1723512c1f6fafa87486b30b995ba69e17
takizawa73 <rtakizawa0703@gmail.com>
2020-01-07 12:27:05 +0900

    タイトルシーンを作ってとりあえず遷移できるように

---
+++ New Unity Project/Assets/Takizawa/Scripts/MySceneManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : Singleton<MySceneManager>
+{
+    public enum E_Scene
+    {
+        TITLE   = 0,
+        SETTING = 1,
+        MAIN    = 2,
+    }
+
+    public E_Scene m_Scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        m_Scene = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(E_Scene scene)
+    {
+        if (scene == E_Scene.SETTING)
+        {
+            SceneManager.LoadScene("FirstSetting");
+        }
+
+        if (scene == E_Scene.MAIN)
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
---
+++ New Unity Project/Assets/Takizawa/Scripts/TitleManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TouchManager.Instance.m_TouchFlag)
+        {
+            if (TouchManager.Instance.m_TouchPos.x < Screen.width / 2)
+            {
+                MySceneManager.Instance.ChangeScene(MySceneManager.E_Scene.SETTING);
+            }
+            else
+            {
+                MySceneManager.Instance.ChangeScene(MySceneManager.E_Scene.MAIN);
+            }
+        }
+    }
+}
