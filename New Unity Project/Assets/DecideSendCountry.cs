76833e633d4f7ee5c03e05c3910970305835bf5e
KYoshida1012 <yoshida-k@keio.jp>
2020-01-08 18:03:07 +0900

    緯度経度計算

---
+++ New Unity Project/Assets/DecideSendCountry.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class DecideSendCountry : MonoBehaviour
+{
+    TextAsset csvFile;
+    List<string[]> csvDatas = new List<string[]>();
+    List<double> Latitudelist = new List<double>();
+    List<double> Longitudelist = new List<double>();
+
+    GameObject Sumaho;
+    CalculatePoint CalPscript;
+
+    int Datanum = 0;
+    double PointDistance = 0.0f;
+    double MaxDistance = 40000000;
+    double NearLat;
+    double NearLon;
+    int Nearnum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sumaho = GameObject.Find("Sumaho");
+        CalPscript = Sumaho.GetComponent<CalculatePoint>();
+
+        csvFile = Resources.Load("CountryList_txt") as TextAsset;
+        StringReader reader = new StringReader(csvFile.text);
+
+        //Debug.Log(csvFile.text);
+
+        //Debug.Log("DecideStart");
+        
+        while (reader.Peek() != -1)
+        {
+            string line = reader.ReadLine();
+            csvDatas.Add(line.Split(','));
+            //Latitudelist.Add(double.Parse(csvDatas[i][1]));
+           // Longitudelist.Add(double.Parse(csvDatas[i][2]));
+            Datanum++;
+            //Debug.Log(line);
+            //Debug.Log(csvDatas[2][2]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DecideSend()
+    {
+        //Debug.Log(csvDatas[2][2]);
+        //Debug.Log(Latitudelist[1] + ", " + Longitudelist[1]);
+
+        int i = 1;
+        while (i < Datanum)
+        {
+            PointDistance = Math.Sqrt((CalPscript.lat2 - double.Parse(csvDatas[i][2])) * (CalPscript.lat2 - double.Parse(csvDatas[i][2])) 
+                + (CalPscript.lon2 - double.Parse(csvDatas[i][3])) * (CalPscript.lon2 - double.Parse(csvDatas[i][3])));
+
+            i++;
+
+            if(PointDistance < MaxDistance)
+            {
+                MaxDistance = PointDistance;
+                NearLat = double.Parse(csvDatas[i][2]);
+                NearLon = double.Parse(csvDatas[i][3]);
+                Nearnum = i;
+                //Debug.Log(csvDatas[i][1]);
+            }
+        }
+        Debug.Log(csvDatas[Nearnum][0] + ", " + NearLat + ", " + NearLon);
+    }
+}
---
+++ New Unity Project/Assets/SumAcceleration.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumAcceleration : MonoBehaviour
+{
+
+    const int HISMAX = 10;      // 履歴の個数
+    const float BORDER = 2.0f;  // しきい値
+    public float SUMx = 0.0f;
+    public float SUMy = 0.0f;
+    public float SUMz = 0.0f;
+    public float SUMall = 0.0f;
+    public float Heading = 0.0f;
+    public double Latitude;
+    public double Longitude;
+    public double Distance;
+    Vector3 center;
+
+    public LocationUpdate update;
+
+    List<Vector3> history = new List<Vector3>(HISMAX);
+
+    // Use this for initialization
+    void Start()
+    {
+        center = transform.position;
+        Input.compass.enabled = true; //コンパス有効化
+        //Input.location.Start(); //位置情報有効化
+        update = gameObject.GetComponent<LocationUpdate>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float scale = 2f;
+        Vector3 dir = Input.acceleration;
+        Vector3 pos = new Vector3(
+            center.x + dir.x * scale,
+            center.y + dir.y * scale,
+            center.z + dir.z * scale
+        );
+        this.transform.position = pos;
+
+        if (history.Count >= HISMAX)
+        {
+            history.RemoveAt(0);
+        }
+        history.Add(pos);
+
+        SUMx += dir.x;
+        SUMy += dir.y;
+        SUMz += dir.z;
+
+        //DrawLines ();
+    }
+
+    private void OnGUI()
+    {
+        Vector3 dir = Input.acceleration;
+        GUI.TextField(new Rect(10, 10, 100, 100), "X = " + dir.x.ToString());
+        GUI.TextField(new Rect(10, 30, 100, 100), "Y = " + dir.y.ToString());
+        GUI.TextField(new Rect(10, 50, 100, 100), "Z = " + dir.z.ToString());
+        GUI.TextField(new Rect(10, 70, 100, 100), "SUMx = " + SUMx.ToString());
+        GUI.TextField(new Rect(10, 90, 100, 100), "SUMy = " + SUMy.ToString());
+        GUI.TextField(new Rect(10, 110, 100, 100), "SUMz = " + SUMz.ToString());
+    }
+
+    /*void DrawLines ()
+    {
+        for (int i = 1; i < history.Count; i++) {
+            float distance = Vector3.Distance (history [i - 1], history [i]);
+            Color col = (distance >= BORDER) ? Color.red : Color.green;
+            Debug.DrawLine (history[i-1], history[i], col, 2.0f);
+        }
+    }*/
+
+    public void DecideAddress()
+    {
+        SUMall = SUMx + SUMy + SUMz;  //加速度の和
+        Distance = SUMall * (-10000);
+        Debug.Log(SUMall);
+        Debug.Log(Distance);
+        //Debug.Log("Distance: " + Distance);
+        Heading = Input.compass.trueHeading; //方位
+        //Debug.Log("Heading: " + Heading);
+        //Latitude = Input.location.lastData.latitude; //緯度取得
+        //Latitude = update.Location.latitude;
+        //Debug.Log(Latitude);
+        //Longitude = Input.location.lastData.longitude; //経度取得
+        //Longitude = update.Location.longitude;
+        //Debug.Log(Longitude);
+        //Distance = 1000000;
+        Heading = 270;
+        Latitude = 35.5557358;
+        Longitude = 139.6537767;
+    }
+}
+
