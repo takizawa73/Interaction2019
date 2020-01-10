4d048c6f9a5007833c6ac12344f0554286e5bd91
KYoshida1012 <yoshida-k@keio.jp>
2020-01-10 15:33:23 +0900

    message input

--- New Unity Project/Assets/ShowResult.cs
+++ New Unity Project/Assets/ShowResult.cs
@@ -8,14 +8,21 @@ public class ShowResult : MonoBehaviour
     private Text ResultText;
     GameObject Resultdeliver;
     ResultDeliver Resultdeliverscript;
+
+    GameObject Textdeliver;
+    TextDeliver Textdeliverscript;
     // Start is called before the first frame update
     void Start()
     {
         Resultdeliver = GameObject.Find("ResultDeliver");
         Resultdeliverscript = Resultdeliver.GetComponent<ResultDeliver>();
 
+        Textdeliver = GameObject.Find("DeliverText");
+        Textdeliverscript = Textdeliver.GetComponent<TextDeliver>();
+
         ResultText = GetComponentInChildren<Text>();
         ResultText.text = "あなたの力で\n" + Resultdeliverscript.deliverString + "に\n届きました";
+        Debug.Log(Textdeliverscript.deliverText);
     }
 
     // Update is called once per frame
